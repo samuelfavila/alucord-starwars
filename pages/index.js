84d0ee31e072b1dc52529327825a8524e4332d6e
samuelfavila <samuel2001.furtadoavila@gmail.com>
2022-01-26 09:32:53 -0300

    Aula 2, useState e useRouter-nxt

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-	return (
-		<style global jsx>{`
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-				list-style: none;
-			}
-			body {
-				font-family: "Open Sans", sans-serif;
-			}
-			/* App fit Height */
-			html,
-			body,
-			#__next {
-				min-height: 100vh;
-				display: flex;
-				flex: 1;
-			}
-			#__next {
-				flex: 1;
-			}
-			#__next > * {
-				flex: 1;
-			}
-			/* ./App fit Height */
-		`}</style>
-	);
-}
 
 function Title(props) {
 	console.log(props);
@@ -63,11 +34,12 @@ function Title(props) {
 
 // export default HomePage;
 export default function PaginaInicial() {
-	const username = "samuelfavila";
+	// const username = "samuelfavila";
+	const [username, setUsername] = React.useState("samuelfavila");
+	const roteamento = useRouter();
 
 	return (
 		<>
-			<GlobalStyle />
 			<Box
 				styleSheet={{
 					display: "flex",
@@ -102,6 +74,12 @@ export default function PaginaInicial() {
 					{/* Formulário */}
 					<Box
 						as="form"
+						onSubmit={function (e) {
+							e.preventDefault();
+							console.log("Submeteu");
+							roteamento.push("/chat");
+							//window.location.href = '/chat';
+						}}
 						styleSheet={{
 							display: "flex",
 							flexDirection: "column",
@@ -123,7 +101,27 @@ export default function PaginaInicial() {
 							{appConfig.name}
 						</Text>
 
+						{/* <input
+							type="text"
+							value={username}
+							onChange={function (event) {
+								console.log("usuario digitou", event.target.value);
+								//Onde está o valor?
+								const valor = event.target.value;
+								//Trocar o valor da variavel
+								setUsername(valor);
+							}}
+						/> */}
+
 						<TextField
+							value={username}
+							onChange={function (event) {
+								console.log("usuario digitou", event.target.value);
+								//Onde está o valor?
+								const valor = event.target.value;
+								//Trocar o valor da variavel
+								setUsername(valor);
+							}}
 							fullWidth
 							textFieldColors={{
 								neutral: {
